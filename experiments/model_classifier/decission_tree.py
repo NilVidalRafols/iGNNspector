997071b63064dffccca451dce644586bfc455081
nil.vidal.rafols <nil.vidal.rafols@est.fib.upc.edu>
2021-04-06 19:45:45 +0300

    decission tree experiment added

---
+++ experiments/model_classifier/decission_tree.py
@@ -0,0 +1,11 @@
+from sklearn import tree
+X = [[0, 0], [1, 1]]
+Y = [0, 1]
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(X, Y)
+
+print(clf.predict([[2., 2.]]))
+
+print(clf.predict_proba([[2., 2.]]))
+
+tree.plot_tree(clf)
